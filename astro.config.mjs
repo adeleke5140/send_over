351aed975630e6aee44a8164ba91152d1e8be0b4
Kenny <adelekekehinde06@gmail.com>
2023-03-18 21:11:01 +0100

    feat: add react plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), react()]
 });
