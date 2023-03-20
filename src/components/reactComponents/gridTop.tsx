6e0b84b52b056bd37665df1a6eb404d17896f3b4
Kenny <adelekekehinde06@gmail.com>
2023-03-20 03:31:49 +0100

    style: add border styles

--- src/components/reactComponents/gridTop.tsx
+++ src/components/reactComponents/gridTop.tsx
@@ -6,12 +6,12 @@ export const GridTop = () => {
     console.log(navigatorEl);
   }, []);
   return (
-    <section className="flex justify-between">
-      <p className="p-6 border-l-2 border-r-2 border-b-2 border-color-dark font-medium text-base">
+    <section className="flex justify-between border-b-2 border-color-dark">
+      <p className="p-6 border-l-2 border-r-2 border-color-dark font-medium text-base hover:bg-color-pink cursor-pointer">
         User:
         <span className="pl-4 text-[32px] font-semibold">Not Logged In</span>
       </p>
-      <p className="p-6 border-l-2 border-r-2 border-b-2 border-color-dark font-medium text-base hover:bg-color-yellow cursor-pointer">
+      <p className="p-6 border-l-2 border-r-2 border-color-dark font-medium text-base hover:bg-color-yellow cursor-pointer">
         <span className="uppercase pl-4 text-[32px] font-semibold">
           {" "}
           Get Access
