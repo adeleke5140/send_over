00c230545a7efaaec27a414ff7b4b85d710639e5
Kenny <adelekekehinde06@gmail.com>
2023-03-20 04:14:03 +0100

    feat: pass link as props to sendLink

--- src/components/react/main.tsx
+++ src/components/react/main.tsx
@@ -6,13 +6,14 @@ import { useState } from "react";
 
 export function Main() {
   const [link, setLink] = useState("");
+
   return (
     <main className="h-full border-t-2 border-color-dark">
       <Nav />
       <div className="flex w-full h-full">
         <SiteLeft setLink={setLink} />
         <section className="flex-1 flex gap-4 bg-color-yellow p-6 pt-24">
-          <SendLink />
+          <SendLink link={link} />
           <ReceiveLink />
         </section>
       </div>
