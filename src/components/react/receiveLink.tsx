f803316d4872c6f52829fcee063338b2b471cb0c
Kenny <adelekekehinde06@gmail.com>
2023-04-02 01:58:51 +0100

    feat: subscribe to db changes

--- src/components/react/receiveLink.tsx
+++ src/components/react/receiveLink.tsx
@@ -1,13 +1,28 @@
-import { Button } from "./button";
+import { useEffect, useState } from "react";
+import { supabase } from "../../supabase/init";
 import { TextArea } from "./textarea";
 
 const ReceiveLink = () => {
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    supabase
+      .channel("links")
+      .on("postgres_changes", { event: "*", schema: "*" }, (data) => {
+        const newValue = data.new.link;
+        setText(newValue);
+      })
+      .subscribe();
+  }, []);
+
   return (
     <div className="flex-1">
-      <TextArea placeholder="Receive Link here..." />
-      <div>
-        <Button name="Copy" />
-      </div>
+      <TextArea
+        placeholder="Receive Link here..."
+        buttonText="Copy"
+        value={text}
+        callback={() => ""}
+      />
     </div>
   );
 };
