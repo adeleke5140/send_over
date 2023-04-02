c13847e651396be4daa194062e8c940959dbb260
Kenny <adelekekehinde06@gmail.com>
2023-04-02 02:37:22 +0100

    fix(style): clean up link in nav

--- src/components/react/nav.tsx
+++ src/components/react/nav.tsx
@@ -1,15 +1,8 @@
-import { useEffect } from "react";
-
 export const Nav = () => {
   return (
-    <section className="flex justify-between border-b-2 border-color-dark">
-      <p className="p-6 border-l-2 border-r-2 border-color-dark font-medium text-base hover:bg-color-pink cursor-pointer">
-        User:
-        <span className="pl-4 text-[32px] font-semibold">Not Logged In</span>
-      </p>
-      <p className="p-6 border-l-2 border-r-2 border-color-dark font-medium text-base hover:bg-color-yellow cursor-pointer">
-        <span className="uppercase pl-4 text-[32px] font-semibold">
-          {" "}
+    <section className="flex items-center  justify-between border-b-2 border-color-dark">
+      <p className="w-full flex items-center justify-center p-6 md:border-l-2 md:border-r-2 border-color-dark font-medium text-base hover:bg-color-yellow cursor-pointer">
+        <span className="uppercase md:pl-4 text-[32px] font-medium text-center">
           Get Access
         </span>
       </p>
