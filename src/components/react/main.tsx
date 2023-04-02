10f31b90ed29e9a79ea9cc105ba27f2e3bde13b3
Kenny <adelekekehinde06@gmail.com>
2023-04-02 02:28:36 +0100

    fix(style): update flex property for mobile devices

--- src/components/react/main.tsx
+++ src/components/react/main.tsx
@@ -8,9 +8,9 @@ export function Main() {
   const [link, setLink] = useState("");
 
   return (
-    <main className="h-full border-t-2 border-color-dark">
+    <main className="border-t-2 border-color-dark">
       <Nav />
-      <div className="flex w-full h-full">
+      <div className="flex flex-col md:flex-row">
         <SiteLeft setLink={setLink} />
         <section className="flex-1 flex gap-4 bg-color-yellow p-6 pt-24">
           <SendLink link={link} />
