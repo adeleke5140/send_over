262be3183a179d632ba53a5215a4fadba901f293
Kenny <adelekekehinde06@gmail.com>
2023-04-02 02:31:32 +0100

    fix(style): update flex property

--- src/components/react/main.tsx
+++ src/components/react/main.tsx
@@ -12,7 +12,7 @@ export function Main() {
       <Nav />
       <div className="flex flex-col md:flex-row">
         <SiteLeft setLink={setLink} />
-        <section className="flex-1 flex gap-4 bg-color-yellow p-6 pt-24">
+        <section className="flex-1 flex flex-col md:flex-row gap-4 bg-color-yellow p-6 pt-24">
           <SendLink link={link} />
           <ReceiveLink />
         </section>
