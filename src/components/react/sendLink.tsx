a05005425f429cf1c2ec37b933fc37126098ce15
Kenny <adelekekehinde06@gmail.com>
2023-04-02 01:40:54 +0100

    feat: insert link into postgresDB

--- src/components/react/sendLink.tsx
+++ src/components/react/sendLink.tsx
@@ -1,18 +1,26 @@
-import { Button } from "./button";
 import { TextArea } from "./textarea";
+import { supabase } from "../../supabase/init";
 
 interface SendLinkProps {
   link: string;
 }
 
 const SendLink = ({ link }: SendLinkProps) => {
+  const sendData = async (value: string) => {
+    const { data, error } = await supabase
+      .from("links")
+      .insert({ link: value });
+  };
+
   return (
-    <div className="flex-1">
-      <TextArea placeholder="Paste link here..." value={link} />
-      <div>
-        <Button name="send" />
-      </div>
-    </div>
+    <form className="flex-1">
+      <TextArea
+        placeholder="Paste link here..."
+        value={link}
+        buttonText="send"
+        callback={sendData}
+      />
+    </form>
   );
 };
 
