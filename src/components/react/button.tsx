fa7d3b089f35a8c9d18ff7a7f860ad31afbf098a
Kenny <adelekekehinde06@gmail.com>
2023-04-02 02:49:20 +0100

    fix(style): update button styles for mobile

--- src/components/react/button.tsx
+++ src/components/react/button.tsx
@@ -9,7 +9,7 @@ const Button = ({ name, callback }: ButtonProps) => {
   return (
     <button
       type="button"
-      className="px-24 text-xl uppercase font-semibold tracking-wide py-8 text-white relative cursor-pointer bold border-transparent"
+      className="px-16 md:px-24 py-6 md:py-8 text-base md:text-xl uppercase font-semibold tracking-wide  text-white relative cursor-pointer bold border-transparent"
       onClick={callback}
     >
       <span className="border-black border-2 border-solid rounded"></span>
