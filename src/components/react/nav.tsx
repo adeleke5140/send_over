629a5a97fe29743b7ba7cfb9f8620c79fb90064e
Kenny <adelekekehinde06@gmail.com>
2023-04-24 20:33:32 +0100

    fix: update nav text

--- src/components/react/nav.tsx
+++ src/components/react/nav.tsx
@@ -2,8 +2,8 @@ export const Nav = () => {
   return (
     <section className="flex items-center  justify-between border-b-2 border-color-dark">
       <p className="w-full flex items-center justify-center p-6 md:border-l-2 md:border-r-2 border-color-dark font-medium text-base hover:bg-color-yellow cursor-pointer">
-        <span className="uppercase md:pl-4 text-[32px] font-medium text-center">
-          Get Access
+        <span className="md:pl-4 text-[32px] font-medium text-center">
+          Real-time text transfer.
         </span>
       </p>
     </section>
